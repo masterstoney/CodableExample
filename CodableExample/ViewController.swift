8c2e03c020f423bd79b2c69b1f7dc197f7c8b038
masterstoney <tgmsstoney@gmail.com>
2019-03-08 08:17:53 -0600

    Initial Commit

---
+++ CodableExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodableExample
+//
+//  Created by Tendaishe Gwini on 08/03/2019.
+//  Copyright © 2019 Tendaishe Gwini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CodableExampleTests/CodableExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodableExampleTests.swift
+//  CodableExampleTests
+//
+//  Created by Tendaishe Gwini on 08/03/2019.
+//  Copyright © 2019 Tendaishe Gwini. All rights reserved.
+//
+
+import XCTest
+@testable import CodableExample
+
+class CodableExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
